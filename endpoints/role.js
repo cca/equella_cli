a65f602d8bd02a60ccbb868221994343f8d013e3
phette23 <phette23@gmail.com>
2015-04-06 14:48:48 -0700

    centralize error handling in a function

--- endpoints/role.js
+++ endpoints/role.js
@@ -1,15 +1,12 @@
 var path = require('path')
 var req = require(path.join(process.cwd(), 'lib', 'req'))
+var handle = require(path.join(process.cwd(), 'lib', 'handle-error'))
 
 module.exports = function (options) {
     if (options.name) {
         options.path = 'name/' + options.name
         req(options, function (err, resp, data) {
-            if (err) throw err
-            if (data.error) {
-                console.error(data.error_description)
-                process.exit(1)
-            }
+            handle(err, data)
 
             console.log(JSON.stringify(data, null, 2))
         })
--- endpoints/user.js
+++ endpoints/user.js
@@ -1,13 +1,19 @@
 var path = require('path')
 var req = require(path.join(process.cwd(), 'lib', 'req'))
+var handle = require(path.join(process.cwd(), 'lib', 'handle-error'))
 
 module.exports = function (options) {
     var query = options.name || options.q
 
+    // @todo should this be another findByName call?
     if (query) {
         options.path = '?q=' + query
         return req(options, function (err, resp, data) {
-            if (err) throw err
+            handle(err, data)
+
+            if (data.results.length === 0) {
+                return console.error('Unable to find', options.name, 'in list of users')
+            }
 
             // only print 1st result
             // @todo: if 2 users begin with the same stem will this cause problems?
