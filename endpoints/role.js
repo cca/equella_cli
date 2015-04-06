e503f0ded3bd327e9feb77a0488d85c5d5529802
phette23 <phette23@gmail.com>
2015-04-06 14:38:40 -0700

    make role endpt handle names a bit better
    also better error handling

--- endpoints/role.js
+++ endpoints/role.js
@@ -3,9 +3,13 @@ var req = require(path.join(process.cwd(), 'lib', 'req'))
 
 module.exports = function (options) {
     if (options.name) {
-        options.path = '?name=' + options.name
+        options.path = 'name/' + options.name
         req(options, function (err, resp, data) {
             if (err) throw err
+            if (data.error) {
+                console.error(data.error_description)
+                process.exit(1)
+            }
 
             console.log(JSON.stringify(data, null, 2))
         })
