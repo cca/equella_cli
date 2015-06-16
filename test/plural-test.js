727fe7725b5a1effba41bb67b259592199faa6cf
phette23 <phette23@gmail.com>
2015-06-16 10:32:20 -0700

    add tests for plural fn

---
+++ test/plural-test.js
@@ -0,0 +1,19 @@
+var pl = require('../lib/plural')
+
+exports['Pluralize words ending with a Y'] = function (test) {
+    test.strictEqual('taxonomies', pl('taxonomy'))
+    test.strictEqual('taxonomies', pl('taxonomy/'))
+    test.done()
+}
+
+exports['Pluralize words ending with a non-Y, non-S letter'] = function (test) {
+    test.strictEqual('roles', pl('role'))
+    test.strictEqual('roles', pl('role/'))
+    test.done()
+}
+
+exports['Do not double pluralize a plural'] = function (test) {
+    test.strictEqual('groups', pl('groups'))
+    test.strictEqual('groups', pl('groups/'))
+    test.done()
+}
