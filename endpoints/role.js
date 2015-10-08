6dfd1870a2350931fc5068a6eb761dc27a21a6d6
phette23 <phette23@gmail.com>
2015-10-08 10:53:21 -0700

    with fix in role endpt in 6.4, can use findByName helper now

--- endpoints/role.js
+++ endpoints/role.js
@@ -1,18 +1,10 @@
 var req = require('../lib/req')
-var handle = require('../lib/handle-error')
+var findByName = require('../lib/find-by-name')
 
 module.exports = function (options) {
-    // @todo can't use findByName because plain "role" endpt
-    // just returns { "start": 0, "length": 23, "available": 23 }
-    // with no results list! bug in EQUELLA API according to support 4/22/15
     if (options.name) {
-        options.path = 'name/' + options.name
-        req(options, function (err, resp, data) {
-            handle(err, data)
-
-            console.log(JSON.stringify(data, null, 2))
-        })
-    } else {
-        req(options)
+        return findByName(options)
     }
+
+    return req(options)
 }
