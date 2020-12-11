df20b4269ef44f9ef97f8587a0f6ca00a9cd3007
phette23 <phette23@gmail.com>
2020-12-11 13:56:10 -0800

    return URL upon item creation
    also fix bug in item route, seems like return value of item
    API route changed from an array to object at some point
    so now we'll handle either format
    also create actual tests for the item route & xpath parsing

--- endpoints/item.js
+++ endpoints/item.js
@@ -1,20 +1,21 @@
-var req = require('../lib/req')
-var xpath = require('xpath')
-var Dom = require('xmldom').DOMParser
+const req = require('../lib/req')
+const xpath = require('xpath')
+const Dom = require('xmldom').DOMParser
 // @todo more to implement in item endpoint, see:
 // apidocs.do#!/item
 
 module.exports = function (options) {
     // xpath expression, if present
-    var xp = options.x || options.xp
+    let xp = options.x || options.xp
 
     if (xp) {
         return req(options, function (err, resp, data) {
             // --text flag means we want the text, not the XML node
             xp = (options.text ? xp + '/text()' : xp)
+            let item = Array.isArray(data) ? data[0] : data
             // EQUELLA item XML, excluding system-generated stuff under /xml/item,
             // is all contained in the "metadata" property
-            var xml = new Dom().parseFromString(data[0].metadata)
+            let xml = new Dom().parseFromString(item.metadata)
             console.log(xpath.select(xp, xml).toString())
         })
     } else {
---
+++ test/item-xpath-test.js
@@ -0,0 +1,55 @@
+/*jshint esversion: 8 */
+// @NOTE: this test requires a test collection with a UUID referenced inside
+// test/fixtures/test-item.js
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+const xpath = require('xpath')
+const Dom = require('xmldom').DOMParser
+const testData = require('./fixtures/test-item.json')
+
+const doc = new Dom().parseFromString(testData.metadata)
+let itemID;
+
+async function createTestItem(cb) {
+    const { stdout, stderr } = await exec('eq item --method post --file test/fixtures/test-item.json')
+    console.error(stderr)
+    // eq returns the item's API URL upon successful creation, it looks like
+    // {config.root}/api/item/{uuid}/{version}/
+    itemID = stdout.split('api/item/')[1]
+    console.log(`Created test item ${stdout}`)
+    cb()
+}
+
+async function testXpath(test) {
+    const xp = '/xml/mods/titleInfo/title'
+    const { stdout, stderr } = await exec(`eq item --xp ${xp} ${itemID}`)
+    console.error(stderr)
+    // returned XML data should be present in fixture metadata
+    test.equals(stdout, xpath.select(xp, doc).toString() + '\n')
+    test.done()
+}
+
+async function testXpathText(test) {
+    const xp = '/xml/mods/titleInfo/subTitle'
+    const { stdout, stderr } = await exec(`eq item --text --xp ${xp} ${itemID}`)
+    console.error(stderr)
+    // only text (no XML tags)
+    test.equals(stdout, xpath.select(`${xp}/text()`, doc).toString() + '\n')
+    test.done()
+}
+
+async function deleteTestItem(cb) {
+    const { stdout, stderr } = await exec(`eq item --method delete ${itemID}`)
+    console.error(stderr)
+    console.log(`Deleted test item ${itemID}`)
+    cb()
+}
+
+module.exports = {
+    setUp: createTestItem,
+    group: {
+        "retrieve XML element from XPath": testXpath,
+        "retrieve text of element from XPath": testXpathText
+    },
+    tearDown: deleteTestItem
+}
