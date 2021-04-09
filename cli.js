041713ffbb2e52cd93a6056b0aa1bc363d18b5ed
Eric Phetteplace <phette23@gmail.com>
2021-04-09 14:35:34 -0700

    var -> const in cli.js

--- cli.js
+++ cli.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-var fs = require('fs')
-var path = require('path')
-var open = require('opn')
-var help = require('./lib/help')
-var defaults = {
+const fs = require('fs')
+const path = require('path')
+const open = require('opn')
+const help = require('./lib/help')
+const defaults = {
     method: 'get',
     raw: true
 }
-var options = require('rc')('equella', defaults)
+let options = require('rc')('equella', defaults)
 require('./lib/validate-options')(options)
 options.root = require('./lib/normalize-root')(options.root)
 
@@ -19,20 +19,20 @@ if (options._[0] === 'help' || options.help || options.h) {
     return help(options)
 // API documentation
 } else if (options.apidocs || options.docs || options._[0] === 'apidocs' || options._[0] === 'docs') {
-    var url = options.root + 'apidocs.do'
+    const url = options.root + 'apidocs.do'
     console.log('Opening %s', url)
     open(url)
     process.exit(0)
 // admin console
 } else if (options.console || options.admin || options._[0] === 'console' || options._[0] === 'admin') {
-    var url = options.root + 'jnlp/admin.jnlp'
+    const url = options.root + 'jnlp/admin.jnlp'
     console.log('Opening %s', url)
     open(url)
     process.exit(0)
 // version number
 } else if (options.version) {
-    var data = fs.readFileSync(path.resolve(__dirname, 'package.json'))
-    var pkg = JSON.parse(data)
+    const data = fs.readFileSync(path.resolve(__dirname, 'package.json'))
+    const pkg = JSON.parse(data)
     console.log(pkg.version)
     process.exit(0)
     // login via browser automatically
@@ -44,7 +44,7 @@ if (options._[0] === 'help' || options.help || options.h) {
         // can pass values to form by referencing their input names in query string
         // "event__" is some magic that triggers an internal EQUELLA function
         // see `onclick=_subev('.authenticate')`` attribute of the log in button
-        var url = options.root + [
+        const url = options.root + [
                 'logon.do?_username=',
                 options.username,
                 '&_password=',
