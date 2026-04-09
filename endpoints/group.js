bbd1a9806c25abda119579cf5c676c7e31e45fd5
phette23 <phette23@gmail.com>
2026-04-09 11:37:31 -0700

    fix: improve req & findByName callback handling
    again, improvements needed for testing

--- endpoints/group.js
+++ endpoints/group.js
@@ -3,7 +3,7 @@ import { findByName } from '../lib/find-by-name.js'
 
 export default function (options, callback) {
     if (options.name) {
-        return findByName(options)
+        return findByName(options, callback)
     } else {
         return req(options, callback)
     }
